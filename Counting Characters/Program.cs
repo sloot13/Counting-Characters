2d20fff1f2daad946b076bbc55ad0521ef24ea57
Kevin Franklin <deerdance72@gmail.com>
2020-06-03 15:08:58 -0400

    final

--- Counting Characters/Program.cs
+++ Counting Characters/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Counting_Characters
 {
@@ -6,7 +7,27 @@ namespace Counting_Characters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            char[] charString = input.ToCharArray();
+            Dictionary<char, int> counter = new Dictionary<char, int>();
+
+            
+            foreach ( char c in charString)
+            {
+                if(counter.ContainsKey(c))
+                {
+                    counter[c]++;
+                } 
+                else
+                {
+                    counter.Add(c, 1);
+                }
+            }
+
+            foreach (KeyValuePair<char, int> letter in counter)
+            {
+                Console.WriteLine(letter.Key + ": " + letter.Value);
+            }
         }
     }
 }
