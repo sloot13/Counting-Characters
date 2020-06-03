020081b68b92a6de92e61834c26d6f063f449713
Kevin Franklin <deerdance72@gmail.com>
2020-06-03 12:35:53 -0400

    Add project files.

---
+++ Counting Characters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Counting_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
